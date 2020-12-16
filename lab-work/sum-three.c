e72c2750f13a7ee36cf808ebfa178a194ea43511
Parth Sharma <parthsharma151@gmail.com>
2020-12-17 01:55:55 +0530

    lectures 8/9

---
+++ lab-work/sum-three.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main () 
+{
+ int a, b, c, sum;
+ printf("Please enter a/b/c:\n");
+ scanf("%d %d %d", &a, &b, &c);
+ sum = a + b + c;
+ printf("Sum is: %d\n", sum);
+ return 0;
+}
